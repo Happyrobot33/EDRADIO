2a214d014d64095845d7c9e341321ad27adbeedf
Happyrobot33 <matthewherber@sbcglobal.net>
2022-01-09 12:32:25 -0500

    Implement PKCE Auth flow

--- SpotifyControl.py
+++ SpotifyControl.py
@@ -1,12 +1,12 @@
 from http.cookiejar import DefaultCookiePolicy
 from logging import exception
 import spotipy
-from spotipy.oauth2 import SpotifyOAuth
+from spotipy.oauth2 import SpotifyPKCE
 from spotipy.util import CLIENT_CREDS_ENV_VARS
 import json
 
 scope = "streaming app-remote-control user-read-playback-state user-read-currently-playing"
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope, client_id='3bdd08cd9fbf4e1b8437467c357494e8', client_secret='3c0f5bb75e314ae99d8ebcc20184f0db', redirect_uri='http://localhost:8080', open_browser=True))
+sp = spotipy.Spotify(auth_manager=SpotifyPKCE(scope=scope, client_id='3bdd08cd9fbf4e1b8437467c357494e8', redirect_uri='http://localhost:8080', open_browser=True))
 
 #Control Functions
 def pause():
